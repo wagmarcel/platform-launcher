fbc6160b0e73675c551e3c2da3b9128855f7074e
Marcel Wagner <wagmarcel@web.de>
2019-11-01 20:08:13 +0100

    Added support to deploy IoT Agent in K3s

--- kubernetes/iot-agent/deploy-testsensor/create.sh
+++ kubernetes/iot-agent/deploy-testsensor/create.sh
@@ -2,6 +2,11 @@
 NODENAME=${1:-NODENAME}
 NAMESPACE=${NAMESPACE:-oisp-devices}
 CONFIG_MAP_NAME=testsensor-config
+if [ "$#" -gt 2 -o "$#" -lt 1 ]; then
+	echo usage: ${0##*/} \<node-name\> [instance-number]
+	echo e.g. ${0##*/} myworker 1
+	exit 1
+fi
 if [ ! -z "$2" ]; then
 	ID=-$2
 fi
@@ -11,9 +16,9 @@ mkdir -p $NODENAME$ID
 echo Copy and adapt the templates
 for file in $(ls *.yaml *.json); do
 	echo Processing $file
-	sed 's|<NODENAME>|'$NODENAME'|g' $file | sed 's|<ID>|'$ID'|g' > $NODENAME$ID/$file
+	sed 's|<NODENAME>|'$NODENAME'|g' $file | sed 's|<ID>|'$ID'|g' | sed 's|<NAMESPACE>|'$NAMESPACE'|g' > $NODENAME$ID/$file
 done
-kubectl create configmap ${CONFIG_MAP_NAME}  --from-file=./$NODENAME$ID/sensorSpecs.json -o yaml --dry-run > $NODENAME$ID/configmap.yaml
+kubectl -n ${NAMESPACE} create configmap ${CONFIG_MAP_NAME}  --from-file=./$NODENAME$ID/sensorSpecs.json -o yaml --dry-run > $NODENAME$ID/configmap.yaml
 cat $NODENAME$ID/configmap.yaml > $NODENAME$ID/all.yaml
 echo --- >> $NODENAME$ID/all.yaml
 cat $NODENAME$ID/pvc.yaml >> $NODENAME$ID/all.yaml
--- kubernetes/iot-agent/global-config/create.sh
+++ kubernetes/iot-agent/global-config/create.sh
@@ -5,4 +5,4 @@ kubectl create namespace ${NAMESPACE} 2>/dev/null || echo "Namespace $NAMESPACE 
 kubectl delete configmap ${CONFIG_MAP_NAME} -n ${NAMESPACE} 2>/dev/null || echo "ConfigMap not existing. Continue."
 kubectl create configmap ${CONFIG_MAP_NAME} --from-file=./config.json -n ${NAMESPACE}
 kubectl delete secret ${SECRET_NAME} -n ${NAMESPACE} 2>/dev/null || echo "Could not delete secret. Continue."
-kubectl create secret generic ${SECRET_NAME} --from-file=./activationCode -n ${NAMESPACE}
+kubectl create secret generic ${SECRET_NAME} --from-file=./activation-code -n ${NAMESPACE}
