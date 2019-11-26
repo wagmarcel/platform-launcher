ee18ff0edd71177b9b23de50b9217de2006c14ec
Marcel Wagner <wagmarcel@web.de>
2019-11-26 12:05:11 +0100

    config for application.yaml

--- kubernetes/iot-agent/deploy-plc/create.sh
+++ kubernetes/iot-agent/deploy-plc/create.sh
@@ -1,6 +1,6 @@
 NODENAME=${1:-NODENAME}
 NAMESPACE=${NAMESPACE:-oisp-devices}
-CONFIG_MAP_NAME=testsensor-config
+CONFIG_MAP_NAME=gateway-config
 if [ "$#" -gt 2 -o "$#" -lt 1 ]; then
 	echo usage: ${0##*/} \<node-name\> [instance-number]
 	echo e.g. ${0##*/} myworker 1
@@ -17,6 +17,7 @@ for file in $(ls *.yaml); do
 	echo Processing $file
 	sed 's|<NODENAME>|'$NODENAME'|g' $file | sed 's|<ID>|'$ID'|g' | sed 's|<NAMESPACE>|'$NAMESPACE'|g' > $NODENAME$ID/$file
 done
+kubectl -n ${NAMESPACE} create configmap $NODENAME${ID}-${CONFIG_MAP_NAME}  --from-file=./$NODENAME$ID/application.yaml -o yaml --dry-run > $NODENAME$ID/configmap.yaml
 cat $NODENAME$ID/configmap.yaml > $NODENAME$ID/all.yaml
 echo --- >> $NODENAME$ID/all.yaml
 cat $NODENAME$ID/pvc.yaml >> $NODENAME$ID/all.yaml
