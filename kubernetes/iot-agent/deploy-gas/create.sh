b36a6b50cfb146d202edcbdeb344d7ae24edf722
Marcel Wagner <wagmarcel@web.de>
2019-11-28 10:32:23 +0100

    Added gas device

---
+++ kubernetes/iot-agent/deploy-gas/create.sh
@@ -0,0 +1,32 @@
+NODENAME=${1:-NODENAME}
+NAMESPACE=${NAMESPACE:-oisp-devices}
+CONFIG_MAP_NAME=app-config
+GATEWAYAPPCONFIG=~/src/IndustryFusion-machine-configs/GMS-DV-U13-300\ 20-2x1/fusiongatewayapp/application.yaml
+GATEWAYAPPCONFIGTARGET=application.yaml
+
+if [ "$#" -gt 2 -o "$#" -lt 1 ]; then
+	echo usage: ${0##*/} \<node-name\> [instance-number]
+	echo e.g. ${0##*/} myworker 1
+	exit 1
+fi
+if [ ! -z "$2" ]; then
+	ID=-$2
+fi
+
+echo copying configs from config repo
+cp "${GATEWAYAPPCONFIG}" ${GATEWAYAPPCONFIGTARGET}
+
+
+echo Creating subdir for node $NODENAME$ID
+mkdir -p $NODENAME$ID
+echo Copy and adapt the templates
+for file in $(ls *.yaml); do
+	echo Processing $file
+	sed 's|<NODENAME>|'$NODENAME'|g' $file | sed 's|<ID>|'$ID'|g' | sed 's|<NAMESPACE>|'$NAMESPACE'|g' > $NODENAME$ID/$file
+done
+kubectl -n ${NAMESPACE} create configmap $NODENAME${ID}-${CONFIG_MAP_NAME}  --from-file=${GATEWAYAPPCONFIGTARGET} -o yaml --dry-run > $NODENAME$ID/configmap.yaml
+cat $NODENAME$ID/configmap.yaml > $NODENAME$ID/all.yaml
+echo --- >> $NODENAME$ID/all.yaml
+cat $NODENAME$ID/pvc.yaml >> $NODENAME$ID/all.yaml
+echo --- >> $NODENAME$ID/all.yaml
+cat $NODENAME$ID/deployment.yaml >> $NODENAME$ID/all.yaml
