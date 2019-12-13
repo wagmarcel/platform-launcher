4a3fc957cc1588d22729d316fc9f41e79140770c
Marcel Wagner <wagmarcel@web.de>
2019-12-13 10:56:57 +0000

    updated staging

--- upgrade-staging.sh
+++ upgrade-staging.sh
@@ -1,5 +1,5 @@
 export KUBECONFIG=/root/.kube/config-oisp-staging.yaml
 export HELM_ARGS="--set stolon.persistence.storageClassName=\"ionos-enterprise-hdd\" --set less_resources=\"false\" "
 export NAMESPACE=oisp-staging
-export DOCKER_TAG=v1.2-beta.1
+export DOCKER_TAG=nightly-2019-12-13
 make upgrade-oisp
