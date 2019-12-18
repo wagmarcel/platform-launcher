37f62539e54b08d0f90b2bf1dcdc5f9567668f4a
Marcel Wagner <wagmarcel@web.de>
2019-12-18 07:29:38 +0100

    Kafka operator experiements

--- util/restart-cluster.sh
+++ util/restart-cluster.sh
@@ -27,6 +27,10 @@ printf "\n"
 printf "\033[1mInstalling k8s operators\n"
 printf -- "------------------------\033[0m\n"
 kubectl create -f https://github.com/minio/minio-operator/blob/master/minio-operator.yaml?raw=true --validate=false
+helm repo add banzaicloud-stable https://kubernetes-charts.banzaicloud.com/
+kubectl create ns kafka
+helm install kafka-operator --namespace=kafka banzaicloud-stable/kafka-operator -f https://raw.githubusercontent.com/banzaicloud/kafka-operator/master/config/samples/example-prometheus-alerts.yaml
+#kubectl create -n kafka -f https://raw.githubusercontent.com/banzaicloud/kafka-operator/master/config/samples/simplekafkacluster.yaml
 printf -- "\033[1mOperators installed successfully.\033[0m\n"
 
 printf "\033[1mReady to deploy OISP\033[0m\n"
