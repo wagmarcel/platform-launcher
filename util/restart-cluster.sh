21aa374d62bc177f4a2e54ad344891bd4e435168
Marcel Wagner <wagmarcel@web.de>
2019-12-29 19:46:34 +0100

    kafka in own namespace

--- util/restart-cluster.sh
+++ util/restart-cluster.sh
@@ -30,7 +30,8 @@ kubectl create -f https://github.com/minio/minio-operator/blob/master/minio-oper
 helm repo add banzaicloud-stable https://kubernetes-charts.banzaicloud.com/
 kubectl create ns kafka
 helm install kafka-operator --namespace=kafka banzaicloud-stable/kafka-operator -f https://raw.githubusercontent.com/banzaicloud/kafka-operator/master/config/samples/example-prometheus-alerts.yaml
-#kubectl create -n kafka -f https://raw.githubusercontent.com/banzaicloud/kafka-operator/master/config/samples/simplekafkacluster.yaml
+kubectl create ns zookeeper
+helm install zookeeper-operator --namespace=zookeeper banzaicloud-stable/zookeeper-operator
 printf -- "\033[1mOperators installed successfully.\033[0m\n"
 
 printf "\033[1mReady to deploy OISP\033[0m\n"
