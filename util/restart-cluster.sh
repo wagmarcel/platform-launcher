c8d2dbbf98973b23b505735ca8d6e32b907b79f7
Marcel Wagner <wagmarcel@web.de>
2020-02-22 22:58:28 +0100

    pushd and popd needed for separating deploy_operator from restart_cluster

--- util/restart-cluster.sh
+++ util/restart-cluster.sh
@@ -1,9 +1,10 @@
 printf "\033[1mStarting k3s\n"
 printf -- "------------\033[0m\n"
+pushd .
 rm -rf ~/k3s
 mkdir ~/k3s
 curl https://raw.githubusercontent.com/rancher/k3s/release/v1.0/docker-compose.yml > ~/k3s/docker-compose.yml
- Compose down is necessary for subsequent runs to succeed
+#Compose down is necessary for subsequent runs to succeed
 cd ~/k3s && sudo docker-compose down -v && sudo docker-compose up -d
 printf "Waiting for k3s to create kubeconfig file\n"
 while [ ! -f ~/k3s/kubeconfig.yaml ]
@@ -23,6 +24,7 @@ printf "\033[1mGot storage classes:\033[0m\n"
 kubectl get storageclass
 printf "\n"
 
-sh deploy_operators.sh
+popd
+./deploy_operators.sh
 
 printf "\033[1mReady to deploy OISP\033[0m\n"
