b3b2171b437167da881d54c42594982b38927471
Marcel Wagner <wagmarcel@web.de>
2020-03-14 13:45:53 +0100

    deploy-latest script update tag

--- deploy-latest.sh
+++ deploy-latest.sh
@@ -1,5 +1,5 @@
 
 export NAMESPACE=oisp-ci
-export KUBECONFIG=~/.kube/config_ci_k3s
-export DOCKER_TAG=nightly-2020-02-18
+export KUBECONFIG=${HOME}/.kube/config_ci_k3s
+export DOCKER_TAG=latest
 make deploy-oisp
