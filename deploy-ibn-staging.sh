dfd249a02082d1a48765073f1fedf4a1b9853654
Marcel Wagner <wagmarcel@web.de>
2020-05-08 13:43:19 +0200

    first deployment

---
+++ deploy-ibn-staging.sh
@@ -0,0 +1,5 @@
+export NAMESPACE=oisp-staging
+export KUBECONFIG=${HOME}/.kube/kubeconfig-ibn4.0.yaml
+export DOCKER_TAG=1.3-beta.1
+export HELM_ARGS="--set stolon.persistence.storageClassName=\"ionos-enterprise-hdd\" --set less_resources=\"false\" --set production=\"true\" "
+make deploy-oisp
