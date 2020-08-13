45c5e359d11cb8dc6c626b31c3e7b712f86af848
Marcel Wagner <wagmarcel@web.de>
2020-08-13 12:09:10 +0200

    pre-staging changes

--- deploy-pre-staging.sh
+++ deploy-pre-staging.sh
@@ -1,5 +1,5 @@
 export NAMESPACE=oisp-staging
-export KUBECONFIG=${HOME}/.kube/kubeconfig-ibn4.0.yaml
+export KUBECONFIG=${HOME}/.kube/config-oisp-staging.yaml
 export DOCKER_TAG=nightly-2020-06-30
 export HELM_ARGS="--set stolon.persistence.storageClassName=\"ionos-enterprise-hdd\" --set less_resources=\"false\" --set production=\"true\" "
 make deploy-oisp
