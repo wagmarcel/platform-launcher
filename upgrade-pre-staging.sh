63b5ead42e49562c4d0946221cae2114fa1f495a
Marcel Wagner <wagmarcel@web.de>
2020-11-03 02:09:06 +0100

    updated tag in upgrade script to v2.0.0-beta.1

--- upgrade-pre-staging.sh
+++ upgrade-pre-staging.sh
@@ -1,5 +1,5 @@
 export NAMESPACE=oisp-staging
 export KUBECONFIG=${HOME}/.kube/config-oisp-staging.yaml
-export DOCKER_TAG=nightly-2020-06-30
+export DOCKER_TAG=v2.0.0-beta.1
 export HELM_ARGS="--set stolon.persistence.storageClassName=\"ionos-enterprise-hdd\" --set less_resources=\"false\" --set production=\"true\" "
 make upgrade-oisp
