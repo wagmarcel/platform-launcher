2932055f45db1d874e459d8d957e94f6ce6fdd1f
Marcel Wagner <wagmarcel@web.de>
2020-11-09 15:58:26 +0100

    Wrong merge for cassandradatacenter - pvc was accidentially removed: fixed

--- upgrade-ibn-staging.sh
+++ upgrade-ibn-staging.sh
@@ -1,5 +1,5 @@
 export NAMESPACE=oisp-staging
 export KUBECONFIG=${HOME}/.kube/kubeconfig-ibn4.0.yaml
-export DOCKER_TAG=nightly-2020-06-30
+export DOCKER_TAG=v2.0.0-beta.1
 export HELM_ARGS="--set stolon.persistence.storageClassName=\"ionos-enterprise-hdd\" --set less_resources=\"false\" --set production=\"true\" "
 make upgrade-oisp
