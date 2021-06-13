fdf9f9d71c64715bd9719a147caf01a7ae2ef568
Marcel Wagner <wagmarcel@web.de>
2021-06-14 00:02:43 +0200

    upgraded to v2.0.3-beta.1

--- upgrade-ibn-staging.sh
+++ upgrade-ibn-staging.sh
@@ -2,6 +2,6 @@ export DOCKERUSER=oispreadonly
 export DOCKERPASS=UCouldGuess01!
 export NAMESPACE=oisp-staging
 export KUBECONFIG=${HOME}/.kube/kubeconfig-ibn4.0.yaml
-export DOCKER_TAG=v2.0.2-beta.1
+export DOCKER_TAG=v2.0.3-beta.1
 export HELM_ARGS="--set stolon.persistence.storageClassName=\"ionos-enterprise-hdd\" --set less_resources=\"false\" --set production=\"true\" "
 make upgrade-oisp
